d9272eaceeb5fc90c0e3ce771e7a46aac13d0ac2
meryemselicioglu <meryemselici2000@gmail.com>
2023-03-01 23:07:06 +0000

    commit changes

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -17,11 +17,12 @@ public class Runner {
      * program.
      *
      * Using the static keyword is an intentional decision. If you are using the static keyword and don't know why,
-     * you are making a mistake. More on static here: https://www.baeldung.com/java-static
+     * you are making a mistake. More on static here: https://www.b aeldung.com/java-static
      *
      * @param in the String value you should change global_var to in.
      */
-    public void changeClassVar(String in){
 
+    public void changeClassVar(String in){
+        Class.global_var = in;
     }
 }
